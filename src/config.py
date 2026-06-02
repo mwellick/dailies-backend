71901effdc5e39ceea642311ac927d851724aa59
Michael <mr.anderson528491@gmail.com>
2026-06-02 22:56:38 +0300

    install dependencies & init db config

---
+++ src/config.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings
+
+
+class AppSettings(BaseSettings):
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str = "postgres"
+    POSTGRES_PORT: int = 5432
+
+    @property
+    def database_url(self) -> str:
+        return (
+            f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}"
+            f"@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        )
+
+
+settings = AppSettings()
