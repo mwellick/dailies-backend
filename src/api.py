d7c32448c65817c2525fb66c6d217aa7f7f6bd3a
Michael <mr.anderson528491@gmail.com>
2026-06-06 22:05:46 +0300

    use connection instead of session for db health check endpoint

--- src/api.py
+++ src/api.py
@@ -1,14 +1,12 @@
 from typing import Any
 from pydantic import BaseModel
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter
 from starlette import status
 from sqlalchemy import text
 
 from fastapi.responses import JSONResponse
-from src.database import get_db_session
 from src.exceptions import CustomHttpException
-
-from sqlalchemy.ext.asyncio import AsyncSession
+from src.database import sessionmanager
 
 
 class ErrorResponse(BaseModel):
@@ -34,10 +32,11 @@ api_router = APIRouter(
 
 
 @api_router.get("/db-health-check", status_code=status.HTTP_200_OK)
-async def db_health_check(db_session: AsyncSession = Depends(get_db_session)):
+async def db_health_check():
     try:
-        await db_session.execute(text("SELECT 1"))
-        return {"message": "OK"}
+        async with sessionmanager.connect() as connection:
+            await connection.execute(text("SELECT 1"))
+            return {"message": "OK"}
     except Exception:
         raise CustomHttpException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
